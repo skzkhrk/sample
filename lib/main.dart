918d68e2958e68d7e0b619e2b8ac94f2c2216385
sakazaki_hiroko <6.sakazaki@gmail.com>
2024-06-07 10:00:55 +0900

    テキスト編集

--- lib/main.dart
+++ lib/main.dart
@@ -4,31 +4,18 @@ void main() {
   runApp(const MyApp());
 }
 
+// 自分で作成したウィジェット
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // マテリアルデザイン用ウィジェット
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
         //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -39,15 +26,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  // Widgetに定義するフィールドは常にfinal
   final String title;
 
   @override
@@ -59,11 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+      // setState()でbuild()を再呼び出し
       _counter++;
       debugPrint("HelloWorld");
     });
@@ -71,41 +46,41 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // マテリアルデザイン用ウィジェット
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+        // テーマに合ったカラー
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        // 色を直接指定もできる
+        // backgroundColor: Colors.lightGreenAccent,
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+        // 中央寄せウィジェット
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+          // テキスト用ウィジェット
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            // 太さを指定
+            const Text(
+              'Bold',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            // スタイルを指定
+            const Text(
+              'Italic',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+            // サイズを指定
+            const Text(
+              'fontSize = 36',
+              style: TextStyle(fontSize: 36),
+            ),
+            // 色を指定
+            const Text(
+              'Red',
+              style: TextStyle(color: Colors.red),
+            ),
             const Text(
               'You have pushed the button this many times:',
             ),
@@ -116,11 +91,12 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+      // アクションボタン用ウィジェット
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), // 末尾カンマはbuild()の自動フォーマットをいい感じにする
     );
   }
 }
